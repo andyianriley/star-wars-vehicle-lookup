5e31cdedb56b9e7b21d600cd1abf2df46d82e48a
andy.riley <andy.riley@autotrder.co.uk>
2018-04-30 20:38:08 +0100

    add DR34D0 vehicle

--- vehicleController.js
+++ vehicleController.js
@@ -1,5 +1,7 @@
 const Vehicles = [{ 'vrm': 'W00K133', 'name':'Corellian YT-1300f light freighter', 'maxSpeed' : '1,050 km/h', 'hyper-drive-rating': 'Class 0.5', 'length' : '34.75'},
-{ 'vrm': 'XW1NG', 'name':'Incom T-65B X-wing starfighter', 'maxSpeed' : '1,050 km/h', 'hyper-drive-rating': 'Class 1', 'length' : '12.5'}]
+{ 'vrm': 'XW1NG', 'name':'Incom T-65B X-wing starfighter', 'maxSpeed' : '1,050 km/h', 'hyper-drive-rating': 'Class 1', 'length' : '12.5'},
+{ 'vrm': 'DR34D0', 'name':'Kuat Executor-class Star Dreadnought', 'maxSpeed' : '100 km/h', 'hyper-drive-rating': 'Class 1', 'length' : '19,000'},
+]
 
 exports.list_all_vehicles = function(req, res) {
     res.json(Vehicles)
